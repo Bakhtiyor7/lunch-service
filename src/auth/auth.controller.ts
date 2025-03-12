8b960641422104ad57e13ae88d2e6f3b0622503e
Bakhtiyor7 <baxtiyor.axatov77@gmail.com>
2025-03-13 00:12:38 +0900

    ✨ feat: add error handling on auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,40 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  InternalServerErrorException,
+  Logger,
+  Post,
+} from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginDto } from './dto/login.dto';
+import { ServiceException } from 'src/error/serviceException.error';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  private readonly logger = new Logger(AuthController.name);
 
   @Post('/signup')
   async createUser(@Body() body: CreateUserDto) {
-    const user = await this.authService.signup(body);
-    return user;
+    try {
+      const user = await this.authService.signup(body);
+      return user;
+    } catch (error: any) {
+      this.logger.error(
+        `Error in createUser AuthController: ${error?.message}`,
+      );
+      if (!(error instanceof ServiceException)) {
+        throw new InternalServerErrorException(error?.message);
+      } else {
+        throw new HttpException(error.message, error.errorCode);
+      }
+    } finally {
+      this.logger.log('Request completed for createUser AuthController');
+    }
   }
 
   @Post('/login')
@@ -21,6 +43,18 @@ export class AuthController {
   @ApiResponse({ status: 401, description: 'Invalid credentials' })
   @ApiResponse({ status: 404, description: 'User not found' })
   async signin(@Body() body: LoginDto) {
-    return this.authService.signin(body);
+    try {
+      const user = await this.authService.signin(body);
+      return user;
+    } catch (error: any) {
+      this.logger.error(`Error in signin AuthController: ${error?.message}`);
+      if (!(error instanceof ServiceException)) {
+        throw new InternalServerErrorException(error?.message);
+      } else {
+        throw new HttpException(error.message, error.errorCode);
+      }
+    } finally {
+      this.logger.log('Request completed for signin AuthController');
+    }
   }
 }
