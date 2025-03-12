0f8d93b918017aa6ed6d917043310942d3aef279
Bakhtiyor7 <baxtiyor.axatov77@gmail.com>
2025-03-12 22:28:10 +0900

    ♻️ refactor: remove unnecessary comments

--- src/guards/jwt-auth.guard.ts
+++ src/guards/jwt-auth.guard.ts
@@ -8,12 +8,10 @@ import { AuthGuard } from '@nestjs/passport';
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   canActivate(context: ExecutionContext) {
-    // You can add custom logic here if needed
     return super.canActivate(context);
   }
 
   handleRequest(err, user, info) {
-    // You can throw custom exceptions based on the error or info
     if (err || !user) {
       throw err || new UnauthorizedException('Authentication required');
     }
